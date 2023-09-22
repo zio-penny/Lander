6d541487e51c62fd981011382e382a3039850f83
C.Butler <zionviller@gmail.com>
2023-09-21 22:59:05 -0700

    Added curves to auto rotation

--- lander/Lander.gd
+++ lander/Lander.gd
@@ -4,11 +4,10 @@ extends RigidBody2D
 @export var MainThrustPower: float = 3072
 @export var StrafeThrustPower: float = 512
 @export var RotationThrustMultiplier: float = 16
-
+@export var PitchCurve: Curve
 var _throttle:float
 var _strafe:float
 var _pitch:float # In radians
-var _targetRotation:float
 	
 func _unhandled_input(event):
 	if event.is_action_pressed("Throttle") || event.is_action_released("Throttle"):
@@ -24,8 +23,7 @@ func _unhandled_input(event):
 	if event.is_action_pressed("PitchRight") || event.is_action_released("PitchRight"):
 		_pitch += event.get_action_strength("PitchRight")
 		
-	_targetRotation = _pitch * (PI / 4)
-
+	
 func _integrate_forces(state):
 	for i in range(state.get_contact_count()):
 		var other = state.get_contact_collider_object(i)
@@ -35,5 +33,10 @@ func _integrate_forces(state):
 	
 	if _throttle > 0 : apply_central_force(Vector2.UP.rotated(rotation) * MainThrustPower * _throttle)
 	if _strafe != 0: apply_central_force(Vector2.RIGHT.rotated(rotation) * StrafeThrustPower * _strafe)
-	apply_torque((_targetRotation - rotation) * (MainThrustPower + StrafeThrustPower) * RotationThrustMultiplier)
+	
+	var _targetRotation = _pitch * (PI / 2)
+	var _rotationDiff = _targetRotation - rotation
+	var _curveX = abs(_rotationDiff) / PI
+	
+	apply_torque(PitchCurve.sample(_curveX) * sign(_rotationDiff) * (MainThrustPower + StrafeThrustPower) * RotationThrustMultiplier)
 	
