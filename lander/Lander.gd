049bccddc53277915b98617e2509bf8f849970c0
C.Butler <zionviller@gmail.com>
2023-09-21 22:17:27 -0700

    added pitch control

--- lander/Lander.gd
+++ lander/Lander.gd
@@ -7,7 +7,8 @@ extends RigidBody2D
 
 var _throttle:float
 var _strafe:float
-var _yaw
+var _pitch:float # In radians
+var _targetRotation:float
 	
 func _unhandled_input(event):
 	if event.is_action_pressed("Throttle") || event.is_action_released("Throttle"):
@@ -17,9 +18,22 @@ func _unhandled_input(event):
 		_strafe = -event.get_action_strength("StrafeLeft")
 	if event.is_action_pressed("StrafeRight") || event.is_action_released("StrafeRight"):
 		_strafe += event.get_action_strength("StrafeRight")
+		
+	if event.is_action_pressed("PitchLeft") || event.is_action_released("PitchLeft"):
+		_pitch = -event.get_action_strength("PitchLeft")
+	if event.is_action_pressed("PitchRight") || event.is_action_released("PitchRight"):
+		_pitch += event.get_action_strength("PitchRight")
+		
+	_targetRotation = _pitch * (PI / 4)
 
 func _integrate_forces(state):
+	for i in range(state.get_contact_count()):
+		var other = state.get_contact_collider_object(i)
+		if other:
+			if other is LandingPad:
+				print(other.name)
+	
 	if _throttle > 0 : apply_central_force(Vector2.UP.rotated(rotation) * MainThrustPower * _throttle)
 	if _strafe != 0: apply_central_force(Vector2.RIGHT.rotated(rotation) * StrafeThrustPower * _strafe)
-	if abs(rotation) > 0.001: apply_torque(-(rotation/PI) * (MainThrustPower + StrafeThrustPower) * RotationThrustMultiplier)
+	apply_torque((_targetRotation - rotation) * (MainThrustPower + StrafeThrustPower) * RotationThrustMultiplier)
 	
