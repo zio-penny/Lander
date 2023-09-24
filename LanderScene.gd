ed7b88dccab707ebfc93c42b6b7d3d0512b6237f
C.Butler <zionviller@gmail.com>
2023-09-23 19:06:06 -0700

    tweaks

--- LanderScene.gd
+++ LanderScene.gd
@@ -1,4 +1,4 @@
-extends Node2D
+class_name LanderScene extends Node2D
 
-@export var lander: Lander
-@export var landingPads : Array[LandingPad]
+@export var GravityVector:Vector2 = Vector2(0, 1)
+@export var GravityScale:float = 32.0
--- scripts/Lander.gd
+++ scripts/Lander.gd
@@ -5,11 +5,14 @@ extends RigidBody2D
 @export var StrafeThrustPower: float = 512
 @export var RotationThrustMultiplier: float = 16
 @export var PitchCurve: Curve
+
 var _throttle:float
 var _strafe:float
 var _pitch:float # In radians
 	
 func _unhandled_input(event):
+	sleeping = false
+	
 	if event.is_action_pressed("Throttle") || event.is_action_released("Throttle"):
 		_throttle = event.get_action_strength("Throttle")
 	
@@ -26,17 +29,18 @@ func _unhandled_input(event):
 	
 func _integrate_forces(state):
 	for i in range(state.get_contact_count()):
-		var other = state.get_contact_collider_object(i)
+		var other:Object = state.get_contact_collider_object(i)
+		
 		if other:
 			if other is LandingPad:
 				print(other.name)
 	
-	if _throttle > 0 : apply_central_force(Vector2.UP.rotated(rotation) * MainThrustPower * _throttle)
-	if _strafe != 0: apply_central_force(Vector2.RIGHT.rotated(rotation) * StrafeThrustPower * _strafe)
+	apply_central_force(Vector2.UP.rotated(rotation) * MainThrustPower * _throttle)
+	apply_central_force(Vector2.RIGHT.rotated(rotation) * StrafeThrustPower * _strafe)
 	
-	var _targetRotation = _pitch * (PI / 2)
-	var _rotationDiff = _targetRotation - rotation
-	var _curveX = abs(_rotationDiff) / PI
+	var _targetRotation:float = _pitch * (PI / 2)
+	var _rotationDiff:float = _targetRotation - rotation
+	var _curveX:float = abs(_rotationDiff) / PI
 	
+	# May need to reduce torque when angular velocity is too high
 	apply_torque(PitchCurve.sample(_curveX) * sign(_rotationDiff) * (MainThrustPower + StrafeThrustPower) * RotationThrustMultiplier)
-	
