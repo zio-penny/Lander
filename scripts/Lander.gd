2223d543b09b62ba5ccb2998f9d53bda968a6ee3
C.Butler <zionviller@gmail.com>
2023-10-01 17:48:40 -0700

    changed rotation to direct torque with damping

--- scripts/Lander.gd
+++ scripts/Lander.gd
@@ -11,6 +11,26 @@ var _throttleDown:float
 var _strafe:float
 var _pitchX:float # In radians
 var _pitchY:float # In radians
+
+@onready var _mainThruster:Node2D = $MainThruster
+@export var _mainThrusterLength:float = 32
+@export var _mainThrusterGEMultiplier:float = 4
+
+@onready var _downhruster:Node2D = $DownThruster
+@onready var _leftThruster:Node2D = $LeftThruster
+@onready var _rightThruster:Node2D = $RightThruster
+
+func _physics_process(delta):
+	var space_state = get_world_2d().direct_space_state
+	
+	var mainThrusterQuery = PhysicsRayQueryParameters2D.create(_mainThruster.global_position, _mainThruster.global_position + Vector2(0, _mainThrusterLength).rotated(rotation))
+	var result = space_state.intersect_ray(mainThrusterQuery)
+	
+	var ratio:float = 0.0
+	
+	if result:
+		var d:Vector2 = result.position - _mainThruster.global_position
+		ratio = 1.0 - (d.length()/_mainThrusterLength)
 	
 func _unhandled_input(event):
 	sleeping = false
@@ -43,17 +63,21 @@ func _integrate_forces(state):
 		if other:
 			if other is LandingPad:
 				pass
+				
 	
-	apply_central_force(Vector2.UP.rotated(rotation) * MainThrustPower * _throttle)
+	
+	apply_central_force(Vector2.UP.rotated(rotation) * (MainThrustPower) * _throttle)
+	
 	apply_central_force(Vector2.DOWN.rotated(rotation) * ManuThrustPower * _throttleDown)
 	apply_central_force(Vector2.RIGHT.rotated(rotation) * ManuThrustPower * _strafe)
 	
-	var targetRotation:float = _pitchX * deg_to_rad(PitchRange)
-	var rotationDiff:float = targetRotation - rotation
-	var shortestAngle:float = fmod((rotationDiff + PI), (2 * PI)) - PI
+	# var targetRotation:float = _pitchX * deg_to_rad(PitchRange)
+	# var rotationDiff:float = targetRotation - rotation
+	# var shortestAngle:float = fmod((rotationDiff + PI), (2 * PI)) - PI
 	# var curveX:float = abs(rotationDiff) / PI
 	
 	# apply_torque(shortestAngle * ManuThrustPower * RotationThrustPower - (angular_velocity * ManuThrustPower * RotationDampingMultiplier))
-	apply_torque(shortestAngle * ManuThrustPower * 32)
-	
+	# apply_torque(shortestAngle * ManuThrustPower * 32)
+	apply_torque(_pitchX * ManuThrustPower * 16)
+	apply_torque(-(angular_velocity * ManuThrustPower * 4))
 	
