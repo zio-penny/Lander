6f2e42d7a426ee551ec8431bea6d991a298c62fd
C.Butler <zionviller@gmail.com>
2023-10-08 16:45:59 -0700

    updated names, began ground effect, tweaked numbers, BROKEN: Controls not working

--- scripts/Pod.gd
+++ scripts/Pod.gd
@@ -1,16 +1,29 @@
-class_name Lander
-extends RigidBody2D
+class_name Pod extends RigidBody2D
+## A player controller for pod game mode
+# May have to generalize for NPCs 
+
 
 @export var MainThrustPower: float = 3072
 @export var ManuThrustPower: float = 512
 @export var RotationThrustPower: float = 16
-@export_range(0, 90) var PitchRange: float = 90
 
 var _throttle:float
 var _throttleDown:float
-var _strafe:float
-var _pitchX:float # In radians
-var _pitchY:float # In radians
+
+var _strafeLeft: float
+var _strafeRight: float
+var _strafe: float:
+	get: return _strafeRight - _strafeLeft 
+
+var _rollL:float # In radians
+var _rollR:float
+var _roll: float:
+	get: return _rollR - _rollL
+	
+var _auxUp:float # In radians
+var _auxDown:float
+var _aux: float:
+	get: return _auxUp - _auxDown
 
 @onready var _mainThruster:Node2D = $MainThruster
 @export var _mainThrusterLength:float = 32
@@ -32,52 +45,51 @@ func _physics_process(delta):
 		var d:Vector2 = result.position - _mainThruster.global_position
 		ratio = 1.0 - (d.length()/_mainThrusterLength)
 	
-func _unhandled_input(event):
-	sleeping = false
-	
+func _unhandled_input(event):	
 	if event.is_action_pressed("Throttle") || event.is_action_released("Throttle"):
 		_throttle = event.get_action_strength("Throttle")
+		print(_throttle)
 		
 	if event.is_action_pressed("ThrottleDown") || event.is_action_released("ThrottleDown"):
 		_throttleDown = event.get_action_strength("ThrottleDown")
 	
 	if event.is_action_pressed("StrafeLeft") || event.is_action_released("StrafeLeft"):
-		_strafe = -event.get_action_strength("StrafeLeft")
+		_strafeLeft = event.get_action_strength("StrafeLeft")
+		
 	if event.is_action_pressed("StrafeRight") || event.is_action_released("StrafeRight"):
-		_strafe += event.get_action_strength("StrafeRight")
+		_strafeRight = event.get_action_strength("StrafeRight")
+	
+	if event.is_action_pressed("RollLeft") || event.is_action_released("RollLeft"):
+		_rollL = -event.get_action_strength("RollLeft")
 		
-	if event.is_action_pressed("PitchLeft") || event.is_action_released("PitchLeft"):
-		_pitchX = -event.get_action_strength("PitchLeft")
-	if event.is_action_pressed("PitchRight") || event.is_action_released("PitchRight"):
-		_pitchX += event.get_action_strength("PitchRight")
+	if event.is_action_pressed("RollRight") || event.is_action_released("RollRight"):
+		_rollR = event.get_action_strength("RollRight")
 		
-	if event.is_action_pressed("PitchUp") || event.is_action_released("PitchUp"):
-		_pitchY = -event.get_action_strength("PitchUp")
-	if event.is_action_pressed("PitchDown") || event.is_action_released("PitchDown"):
-		_pitchY += event.get_action_strength("PitchDown")
+	if event.is_action_pressed("AuxUp") || event.is_action_released("AuxUp"):
+		_auxUp = -event.get_action_strength("AuxUp")
+	if event.is_action_pressed("AuxDown") || event.is_action_released("AuxDown"):
+		_auxDown = event.get_action_strength("AuxDown")
 	
 func _integrate_forces(state):
-	for i in range(state.get_contact_count()):
-		var other:Object = state.get_contact_collider_object(i)
-		
-		if other:
-			if other is LandingPad:
-				pass
-				
+#	for i in range(state.get_contact_count()):
+#		var other:Object = state.get_contact_collider_object(i)
+#
+#		if other:
+#			if other is LandingPad:
+#				pass
 	
-	
-	apply_central_force(Vector2.UP.rotated(rotation) * (MainThrustPower) * _throttle)
-	
+	apply_central_force(Vector2.UP.rotated(rotation) * (MainThrustPower) * _throttle)	
 	apply_central_force(Vector2.DOWN.rotated(rotation) * ManuThrustPower * _throttleDown)
+	
 	apply_central_force(Vector2.RIGHT.rotated(rotation) * ManuThrustPower * _strafe)
 	
-	# var targetRotation:float = _pitchX * deg_to_rad(PitchRange)
+	# var targetRotation:float = _rollL * deg_to_rad(PitchRange)
 	# var rotationDiff:float = targetRotation - rotation
 	# var shortestAngle:float = fmod((rotationDiff + PI), (2 * PI)) - PI
 	# var curveX:float = abs(rotationDiff) / PI
 	
 	# apply_torque(shortestAngle * ManuThrustPower * RotationThrustPower - (angular_velocity * ManuThrustPower * RotationDampingMultiplier))
 	# apply_torque(shortestAngle * ManuThrustPower * 32)
-	apply_torque(_pitchX * ManuThrustPower * 16)
+	apply_torque(_roll * RotationThrustPower * 16)
 	apply_torque(-(angular_velocity * ManuThrustPower * 4))
 	
---
+++ scripts/PodGameMode.gd
@@ -0,0 +1,23 @@
+class_name PodGameMode extends Node2D
+## Owns all nodes used in pod exploration scenes
+## and manages the mode's global settings
+
+## Global gravity direction. Down = (0,1)
+@export var GravityVector:Vector2 = Vector2(0,1)
+## Global gravity scale. Earth = 1000.0, Moon = 100.0
+@export var GravityScale:float = 100.0
+ 
+@onready var spaceRID:RID = get_viewport().find_world_2d().space 
+
+func _ready():
+	updateGravityScale(GravityScale)
+	updateGravityVector(GravityVector)
+	
+## Takes in a float and updates the PhysicsServer
+func updateGravityScale(scale) -> void:
+	PhysicsServer2D.area_set_param(spaceRID, PhysicsServer2D.AREA_PARAM_GRAVITY, scale)
+	print("Gravity Scale Updated: ", PhysicsServer2D.area_get_param(spaceRID, PhysicsServer2D.AREA_PARAM_GRAVITY))
+
+func updateGravityVector(vector:Vector2) -> void:
+	PhysicsServer2D.area_set_param(spaceRID, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, vector)
+	print("Gravity Vector Updated: ", PhysicsServer2D.area_get_param(spaceRID, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR))
